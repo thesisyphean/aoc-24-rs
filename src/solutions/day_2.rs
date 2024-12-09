d27ed48c0ea70f310fb15970e4612979c1a77e4b
TheSisyphean <thesisyphean256@gmail.com>
2024-12-10 00:36:16 +0200

    Minor formatting changes, updated template

--- src/solutions/day_2.rs
+++ src/solutions/day_2.rs
@@ -81,7 +81,8 @@ mod tests {
 9 7 6 2 1
 1 3 2 4 5
 8 6 4 4 1
-1 3 6 7 9";
+1 3 6 7 9
+";
 
     #[test]
     fn first_solution() {
--- src/template.rs
+++ src/template.rs
@@ -1,9 +1,26 @@
-pub fn solve_first(input: &str) -> i32 {
+pub fn solve_first(_input: &str) -> i32 {
     // TODO
     -1
 }
 
-pub fn solve_second(input: &str) -> i32 {
+pub fn solve_second(_input: &str) -> i32 {
     // TODO
     -1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = "";
+
+    #[test]
+    fn first_solution() {
+        assert_eq!(solve_first(TEST_INPUT), -1);
+    }
+
+    #[test]
+    fn second_solution() {
+        assert_eq!(solve_second(TEST_INPUT), -1);
+    }
 }
